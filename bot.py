158562adb0e7d3e4af05d77388e8cbc954adcbb7
vasshu <165775987+vedaantbisht@users.noreply.github.com>
2024-07-29 13:00:03 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+import requests
+import json
+
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print("logged on as {0}!" .format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user: 
+            return
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())   
+
+intents = discord.Intents.default()
+intents.message_content = True 
+ 
+client = MyClient(intents = intents)
+client.run("MTI2NzA2NjYzMzQ4NzI1NzYyMg.GNt1gM.FWWo8rj9g5urR3sMeJ8u9ZItltJF1MlN9kYnyI")
